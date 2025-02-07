c7ed06e3d79a5c579fddc93be804bf7cadd5a935
Luiz Kim <luiz.kim@controleonline.com>
2025-02-07 14:10:25 -0300

    Redux

---
+++ src/store/products/product_category/index.js
@@ -0,0 +1,21 @@
+import * as actions from "@controleonline/ui-default/src/store/default/actions";
+import * as getters from "@controleonline/ui-default/src/store/default/getters";
+import mutations from "@controleonline/ui-default/src/store/default/mutations";
+import Formatter from "@controleonline/ui-common/src/utils/formatter.js";
+
+export default {
+  namespaced: true,
+  state: {
+    resourceEndpoint: "product_categories",
+    isLoading: false,
+    error: "",
+    violations: null,
+    totalItems: 0,
+    filters: {},
+    columns: [
+    ],
+  },
+  actions,
+  getters,
+  mutations,
+};
