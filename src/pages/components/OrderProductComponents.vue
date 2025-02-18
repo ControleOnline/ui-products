3807e3470c175b8e9146bea0778e4a40727c89dd
Luiz Kim <luiz.kim@controleonline.com>
2025-02-18 13:26:59 -0300

    Update

---
+++ src/pages/components/OrderProductComponents.vue
@@ -0,0 +1,65 @@
+<template>
+  <q-list  separator>
+    <div v-for="(group, groupId) in groups" :key="groupId">
+      <span>{{ group.productGroup }}</span>
+      <q-item v-for="product in group.products" :key="product.id">
+        <q-item-section side>
+          <q-badge :label="'x' + product.quantity" color="primary" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ product.product.product }}</q-item-label>
+          <q-item-label caption>Preço: R$ {{ product.price }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </div>
+  </q-list>
+</template>
+<script>
+import { mapActions, mapGetters } from "vuex";
+
+export default {
+  components: {},
+  props: {
+    order_product: {
+      default: null,
+    },
+  },
+  data() {
+    return {
+      groups: [],
+    };
+  },
+
+  created() {
+    this.onRequest();
+  },
+
+  methods: {
+    ...mapActions({
+      getOrderProducts: "product_orders/getItems",
+    }),
+    onRequest() {
+      let filter = {
+        order: this.order_product.order["@id"],
+        parentProduct: this.order_product.product["@id"],
+        orderProduct: this.order_product["@id"],
+      };
+
+      this.getOrderProducts(filter)
+        .then((products) => {
+          let grouped = products.reduce((acc, product) => {
+            if (!product.productGroup) return acc;
+            const groupId = product.productGroup.id;
+            if (!acc[groupId]) acc[groupId] = product.productGroup;
+            if (!acc[groupId]["products"]) acc[groupId]["products"] = [];
+            acc[groupId]["products"].push(product);
+            return acc;
+          }, {});
+
+          this.groups = grouped;
+        })
+        .finally(() => {});
+    },
+  },
+};
+</script>
