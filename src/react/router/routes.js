bc2c7909441f4ba83ba32e7da01116838e146a36
Luiz Kim <luiz.kim@controleonline.com>
2025-03-12 16:42:41 -0300

    Update

---
+++ src/react/router/routes.js
@@ -0,0 +1,11 @@
+import AddProductToOrder from "@controleonline/ui-products/src/react/components/products/addProductToOrder";
+
+const productsRoutes = [
+  {
+    name: "AddProductToOrder",
+    component: AddProductToOrder,
+    options: { headerShown: true, title: "Adicionar Produto ao Pedido" },
+  },
+];
+
+export default productsRoutes;
