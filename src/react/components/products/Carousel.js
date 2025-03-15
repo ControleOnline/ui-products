a5619f35e3b1cac957282171c1799ab05fc581cc
Luiz Kim <luiz.kim@controleonline.com>
2025-03-15 12:49:35 -0300

    Update

--- src/react/components/products/Carousel.js
+++ src/react/components/products/Carousel.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { View, FlatList, Image } from 'react-native';
+import {View, FlatList, Image} from 'react-native';
 import css from '@controleonline/ui-orders/src/react/css/orders';
 
-const Carousel = ({ images }) => {
-  const { styles } = css();
+const Carousel = ({images}) => {
+  const {styles} = css();
 
-  const renderItem = ({ item }) => {
-    const imageUrl = `https://api.controleonline.com/files/${item.file.id}/download?t=${Date.now()}`;
+  const renderItem = ({item}) => {
+    const imageUrl = `https://api.controleonline.com/files/${item.file.id}/download`;
 
     return (
-      <View style={[styles.slide, { width: 100, height: 100 }]}>
+      <View style={[styles.slide, {width: 100, height: 100}]}>
         <Image
-          source={{ uri: imageUrl }}
-          style={[styles.image, { width: '100%', height: '100%' }]}
+          source={{uri: imageUrl}}
+          style={[styles.image, {width: '100%', height: '100%'}]}
           resizeMode="cover"
         />
       </View>
@@ -20,11 +20,11 @@ const Carousel = ({ images }) => {
   };
 
   return (
-    <View style={[styles.slidecontainer, { width: '100%', height: '100%' }]}>
+    <View style={[styles.slidecontainer, {width: '100%', height: '100%'}]}>
       <FlatList
         data={images}
         renderItem={renderItem}
-        keyExtractor={(item) => item.id.toString()}
+        keyExtractor={item => item.id.toString()}
         horizontal
         pagingEnabled
         showsHorizontalScrollIndicator={false}
