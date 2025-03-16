8e3344407d9f0f83211f0dba22c84ea7868891f1
Luiz Kim <luiz.kim@controleonline.com>
2025-03-16 00:36:21 -0300

    Update

--- src/react/css/products.js
+++ src/react/css/products.js
@@ -13,6 +13,13 @@ const css = () => {
       marginBottom: 10,
       color: '#333',
     },
+    btnAdd: {
+      flex: 1,
+      color: '#fff',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: colors['primary'],
+    },
     boxWrap: {
       backgroundColor: '#fff',
       elevation: 3,
