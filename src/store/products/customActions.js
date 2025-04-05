69236cba006d2308bd4ed9bd10d678bdcb2eec23
Luiz Kim <luiz.kim@controleonline.com>
2025-04-05 20:36:11 -0300

    Update

---
+++ src/store/products/customActions.js
@@ -0,0 +1,44 @@
+import {api} from '@controleonline/ui-common/src/api';
+import * as types from '@controleonline/ui-default/src/store/default/mutation_types';
+
+export function printPurchasingSuggestion({commit}, params = {}) {
+  commit(types.SET_ISLOADING);
+
+  const options = {
+    method: 'POST',
+    body: params,
+  };
+
+  return api
+    .fetch('/orders/purchasing-suggestion/print', options)
+
+    .then(data => {
+      commit(types.SET_ISLOADING, false);
+
+      return data;
+    })
+    .catch(e => {
+      commit(types.SET_ISLOADING, false);
+
+      commit(types.SET_ERROR, e.message);
+      throw e;
+    });
+}
+
+export function getPurchasingSuggestion({commit}, params = {}) {
+  commit(types.SET_ISLOADING);
+
+  return api
+    .fetch('/products/purchasing-suggestion', {params: params})
+    .then(data => {
+      commit(types.SET_ISLOADING, false);
+
+      return data;
+    })
+    .catch(e => {
+      commit(types.SET_ISLOADING, false);
+
+      commit(types.SET_ERROR, e.message);
+      throw e;
+    });
+}
--- src/store/products/index.js
+++ src/store/products/index.js
@@ -2,6 +2,7 @@ import * as actions from '@controleonline/ui-default/src/store/default/actions';
 import * as getters from '@controleonline/ui-default/src/store/default/getters';
 import mutations from '@controleonline/ui-default/src/store/default/mutations';
 import Formatter from '@controleonline/ui-common/src/utils/formatter.js';
+import * as customActions from './customActions';
 
 export default {
   namespaced: true,
@@ -148,7 +149,7 @@ export default {
       },
     ],
   },
-  actions,
+  actions: {...actions, ...customActions},
   getters,
   mutations,
 };
