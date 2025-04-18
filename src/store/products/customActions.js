955c4ea4fef7c86770e72a9d67742a32a7c743da
Luiz Kim <luiz.kim@controleonline.com>
2025-04-18 19:45:34 -0300

    Update

--- src/store/products/customActions.js
+++ src/store/products/customActions.js
@@ -1,52 +1,6 @@
 import {api} from '@controleonline/ui-common/src/api';
 import * as types from '@controleonline/ui-default/src/store/default/mutation_types';
 
-export function printPurchasingSuggestion({commit}, params = {}) {
-  commit(types.SET_ISLOADING);
-
-  const options = {
-    method: 'POST',
-    body: params,
-  };
-
-  return api
-    .fetch('/products/purchasing-suggestion/print', options)
-
-    .then(data => {
-      commit(types.SET_ISLOADING, false);
-
-      return data;
-    })
-    .catch(e => {
-      commit(types.SET_ISLOADING, false);
-
-      commit(types.SET_ERROR, e.message);
-      throw e;
-    });
-}
-
-
-export function printInventory({commit}, params = {}) {
-  commit(types.SET_ISLOADING);
-
-  const options = {
-    method: 'POST',
-    body: params,
-  };
-
-  return api
-    .fetch('/products/inventory/print', options)
-    .then(data => {
-      commit(types.SET_ISLOADING, false);
-      return data;
-    })
-    .catch(e => {
-      commit(types.SET_ISLOADING, false);
-
-      commit(types.SET_ERROR, e.message);
-      throw e;
-    });
-}
 
 
 export function getInventory({commit}, params = {}) {
